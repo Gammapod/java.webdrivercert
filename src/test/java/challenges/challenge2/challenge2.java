32f9cc6f0faa91a81814fbf70e59ea145ad8df12
Benjamin.henry.hill <Benjamin.henry.hill@churchofjesuschrist.org>
2021-01-04 16:29:59 -0700

    Finished a test for challenge2; getting webdriver to wait for the search to finish was a pain.

--- src/test/java/challenges/challenge2/challenge2.java
+++ src/test/java/challenges/challenge2/challenge2.java
@@ -1,17 +1,26 @@
 package challenges.challenge2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.util.List;
 
 public class challenge2 {
 
     public WebDriver driver;
+    public WebDriverWait wait;
 
     @BeforeClass
     public void startClass() {
         System.setProperty("webdriver.chrome.driver", "./bin/chromedriver.exe");
         driver =  new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterClass
@@ -20,9 +29,16 @@ public class challenge2 {
     }
 
     @Test()
-    public void porscheIsAnExoticCar() {
-//      todo: go to copart.com
-//      todo: search for exotics
-//      todo: verify porsche is in the list of cars
+    public void porscheIsAnExoticCar() throws Exception {
+        driver.get("https://www.copart.com");
+        WebElement searchText = driver.findElement(By.id("input-search"));
+        WebElement searchButton = driver.findElement(By.xpath("//button[@data-uname='homepageHeadersearchsubmit']"));
+
+        searchText.sendKeys("exotics");
+        searchButton.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//table[@id='serverSideDataTable']//td")));
+
+        List<WebElement> porscheCells = driver.findElements(By.xpath("//span[@data-uname='lotsearchLotmake'][text()[contains(.,'PORSCHE')]]"));
+        Assert.assertFalse(porscheCells.isEmpty(), "Searching for 'exotics' should have returned at least one porsche");
     }
 }
