35b03533c319b55138a1b3ceb9ee813719229da5
Benjamin.henry.hill <Benjamin.henry.hill@churchofjesuschrist.org>
2021-01-11 09:54:06 -0700

    Removed the todo in challenge3; since the challenge7 description mentions challenge3, I think I did use the correct section after all.

--- src/test/java/challenges/challenge3/challenge3.java
+++ src/test/java/challenges/challenge3/challenge3.java
@@ -11,7 +11,6 @@ public class challenge3 extends challengeBase {
     @Test()
     public void popularMakesHaveLinks() {
         driver.get("https://www.copart.com");
-//            todo: make sure this is the correct section for this challenge
         List<WebElement> makes = driver.findElements(By.xpath("//div[@id='tabMakes']//a"));
         for (WebElement make : makes) {
             System.out.println(make.getAttribute("innerText") + "- " + make.getAttribute("href"));
