67eec8f0500842e979bc9a590f46c2df8b9b9412
Benjamin.henry.hill <Benjamin.henry.hill@churchofjesuschrist.org>
2021-01-12 09:30:15 -0700

    I moved the word maps to the class level, since they're static. Now they can just be defined once instead of every time writtenNumber calls itself.

--- src/test/java/challenges/challenge4/challenge4.java
+++ src/test/java/challenges/challenge4/challenge4.java
@@ -1,6 +1,7 @@
 package challenges.challenge4;
 
 import com.google.common.base.Splitter;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -8,6 +9,10 @@ import java.util.HashMap;
 import java.util.List;
 
 public class challenge4 {
+    HashMap<Integer, String> singleDigits = new HashMap<>();
+    HashMap<Integer, String> doubleDigits = new HashMap<>();
+    HashMap<Integer, String> teens = new HashMap<>();
+    HashMap<Integer, String> magnitude = new HashMap<>();
 
     @Test()
     public void writeOutFibonacci() {
@@ -19,45 +24,6 @@ public class challenge4 {
     }
 
     public String writtenNumber(Integer inputNumber) {
-        HashMap<Integer, String> singleDigits = new HashMap<>();
-        HashMap<Integer, String> doubleDigits = new HashMap<>();
-        HashMap<Integer, String> teens = new HashMap<>();
-        HashMap<Integer, String> magnitude = new HashMap<>();
-        singleDigits.put(0, "");
-        singleDigits.put(1, " one");
-        singleDigits.put(2, " two");
-        singleDigits.put(3, " three");
-        singleDigits.put(4, " four");
-        singleDigits.put(5, " five");
-        singleDigits.put(6, " six");
-        singleDigits.put(7, " seven");
-        singleDigits.put(8, " eight");
-        singleDigits.put(9, " nine");
-        teens.put(10, " ten");
-        teens.put(11, " eleven");
-        teens.put(12, " twelve");
-        teens.put(13, " thirteen");
-        teens.put(14, " fourteen");
-        teens.put(15, " fifteen");
-        teens.put(16, " sixteen");
-        teens.put(17, " seventeen");
-        teens.put(18, " eighteen");
-        teens.put(19, " nineteen");
-        doubleDigits.put(0, "");
-        doubleDigits.put(2, " twenty");
-        doubleDigits.put(3, " thirty");
-        doubleDigits.put(4, " forty");
-        doubleDigits.put(5, " fifty");
-        doubleDigits.put(6, " sixty");
-        doubleDigits.put(7, " seventy");
-        doubleDigits.put(8, " eighty");
-        doubleDigits.put(9, " ninety");
-        magnitude.put(0, "");
-        magnitude.put(1, " thousand");
-        magnitude.put(2, " million");
-        magnitude.put(3, " billion");
-        magnitude.put(4, " trillion");
-
         String numberString = inputNumber.toString();
         List<Integer> digitsList = new ArrayList<>();
         for (char digit : numberString.toCharArray()) {
@@ -99,5 +65,43 @@ public class challenge4 {
             magnitudeList.add(Integer.parseInt(substringBuilder.toString()));
         }
         return magnitudeList;
+    }
+
+    @BeforeClass
+    public void startClass() {
+        singleDigits.put(0, "");
+        singleDigits.put(1, " one");
+        singleDigits.put(2, " two");
+        singleDigits.put(3, " three");
+        singleDigits.put(4, " four");
+        singleDigits.put(5, " five");
+        singleDigits.put(6, " six");
+        singleDigits.put(7, " seven");
+        singleDigits.put(8, " eight");
+        singleDigits.put(9, " nine");
+        teens.put(10, " ten");
+        teens.put(11, " eleven");
+        teens.put(12, " twelve");
+        teens.put(13, " thirteen");
+        teens.put(14, " fourteen");
+        teens.put(15, " fifteen");
+        teens.put(16, " sixteen");
+        teens.put(17, " seventeen");
+        teens.put(18, " eighteen");
+        teens.put(19, " nineteen");
+        doubleDigits.put(0, "");
+        doubleDigits.put(2, " twenty");
+        doubleDigits.put(3, " thirty");
+        doubleDigits.put(4, " forty");
+        doubleDigits.put(5, " fifty");
+        doubleDigits.put(6, " sixty");
+        doubleDigits.put(7, " seventy");
+        doubleDigits.put(8, " eighty");
+        doubleDigits.put(9, " ninety");
+        magnitude.put(0, "");
+        magnitude.put(1, " thousand");
+        magnitude.put(2, " million");
+        magnitude.put(3, " billion");
+        magnitude.put(4, " trillion");
     }
 }
